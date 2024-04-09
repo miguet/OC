580532454822e00f786de4bce2438e4b7c49b274
Serge Miguet <Serge.Miguet@univ-lyon2.fr>
2024-04-09 10:55:43 +0200

    Initial Commit

---
+++ OC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OC
+//
+//  Created by Serge Miguet on 01/04/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var oc2: [Any]!
+    
+    @IBOutlet var oc: [UIButton]!
+    
+    @IBAction func clic(_ sender: Any) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
